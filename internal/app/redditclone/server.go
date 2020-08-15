bd172c6e54582ab6f4b2db6cb54aed86a01824c3
amyasnikov <aleksei.miasnikov@bg.market>
2020-08-15 03:34:59 +0300

    golint

--- internal/app/redditclone/server.go
+++ internal/app/redditclone/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/asmyasnikov/redditclone/api"
 )
 
+// Run run server with configuration cfg
 func Run(cfg *api.Configuration) error {
 	fmt.Println(cfg.Server.HTTPListen)
 	return nil
